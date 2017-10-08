c9b7814a6f8865661d497a45b3a6f35f2580a434
Darrick <dmckirnan@hotmail.com>
2017-10-08 12:06:59 -0700

    moved fetch from didmount to index.js

--- client/index.js
+++ client/index.js
@@ -5,9 +5,10 @@ import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
 import App from './components/App.jsx';
+import { fetchArticles } from './actions/index';
 
 const store = configureStore();
-
+store.dispatch(fetchArticles());
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-undef */
 render(
