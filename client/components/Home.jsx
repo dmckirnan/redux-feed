8ef2d19399ffd10f04dcdc4fcf29913d8bc2ccb0
Darrick <dmckirnan@hotmail.com>
2017-10-09 11:43:37 -0700

    removed footer

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Feed from '../containers/Feed.jsx';
-import FooterContainer from './FooterContainer.jsx';
 
 const Home = () => (
   <div>
     <Feed />
   </div>
 );
-
-// <FooterContainer />
 
 export default Home;
